e15168f3d1d2ee33dff3c83f19049a1d3528b916
Sudais main <sudaisabubakar6754@gmail.com>
2025-08-05 01:04:44 +0100

    Create support.js

---
+++ pages/support.js
@@ -0,0 +1,13 @@
+export default function Support() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-950 text-center text-white p-6">
+      <h1 className="text-4xl font-bold mb-6">Need Help?</h1>
+      <p className="text-lg mb-2">
+        💬 Telegram: <a className="text-blue-400 underline" href="https://t.me/deekingbeyond">@deekingbeyond</a>
+      </p>
+      <p className="text-lg">
+        📱 WhatsApp: <a className="text-green-400 underline" href="https://wa.me/447796179042">+44 7796 179042</a>
+      </p>
+    </div>
+  );
+    }
