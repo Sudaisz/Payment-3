7d9d3afc0035d9cdc29de8468ff78b8555f85634
Sudais main <sudaisabubakar6754@gmail.com>
2025-08-05 01:05:12 +0100

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-center text-white p-6">
+      <h1 className="text-4xl font-bold mb-4">📩 Contact Us</h1>
+      <p className="text-lg">📧 Email: <span className="text-blue-400">beyondteam@mail.com</span></p>
+    </div>
+  );
+}
